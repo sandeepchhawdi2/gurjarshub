bc8cb64a31b035f23700ab391d90d61b99a494a1
sandeepchhawdi <sandeepchhawdi@sg3plcpnl0025.prod.sin3.secureserver.net>
2017-09-10 08:40:32 -0700

    first commit

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" 
+      href="{{ asset('images/favicon.png') }}">
+
+    <!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="{{ asset('plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/bootstrap/css/bootstrap-theme.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/guest.css') }}" rel="stylesheet">
+    //google adsense code
+    	<!--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+	  (adsbygoogle = window.adsbygoogle || []).push({
+	    google_ad_client: "ca-pub-8902331096208978",
+	    enable_page_level_ads: true
+	  });
+	</script>-->
+</head>
+<body>
+
+<div class="banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                Discover your surroundings. Share your hobbies. Meet new gurjars with GurjarsHub!
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <a href="{{ url('/') }}" class="logo">
+                
+            </a>
+        </div>
+
+        <div class="col-md-6">
+
+
+            <div class="tab_container">
+                <input id="tab1" type="radio" name="tabs" {{ old('tab') != 'register' ? 'checked' : '' }} class="radio_hidden">
+                <label for="tab1" class="head"><i class="fa fa-user"></i><span>LOGIN</span></label>
+
+                <input id="tab2" type="radio" name="tabs" {{ old('tab') == 'register' ? 'checked' : '' }} class="radio_hidden">
+                <label for="tab2" class="head"><i class="fa fa-user-plus"></i><span>SIGN UP</span></label>
+
+                <div class="contents">
+                    <section id="content1" class="tab-content">
+
+                        @include('auth.login')
+
+                    </section>
+
+                    <section id="content2" class="tab-content">
+                        @include('auth.register')
+
+                    </section>
+                </div>
+            </div>
+
+
+
+        </div>
+
+    </div>
+
+
+    @include('widgets.footer')
+</div>
+
+
+<!-- Scripts -->
+<script src="{{ asset('plugins/jquery/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/widgets/footer.blade.php
@@ -0,0 +1,19 @@
+<div class="footer">
+    <div class="row">
+        <div class="col-sm-6">
+        </div>
+        <div class="col-sm-6 text-right">
+            gurjarshub.com © 2017 All rights reserved.
+        </div>
+    </div>
+</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-106131319-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
